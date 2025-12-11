4f541a1c517487283903c7ff169f2406d9025b8f
Novox <113373665+DragonCraft5@users.noreply.github.com>
2025-12-11 21:18:07 +0100

    Enhance monitor utils with color coding and percentage calculation
    Added color variables and calcPercentage function for dynamic text color based on percentage.

--- monUtils.lua
+++ monUtils.lua
@@ -1,9 +1,13 @@
 local monitor = peripheral.find("monitor")
+
 local monXSize, monYSize = monitor.getSize()
 
 local textColor = colors.white
 local backgroundColor = colors.black
 
+local color1 = colors.green
+local color2 = colors.yellow
+local color3 = colors.red
 
 
 -- monitor utils
@@ -18,14 +22,30 @@ local function drawCenteredText(y, text)
     local x = math.floor((monXSize - textLength) / 2) + 1
     monitor.setCursorPos(x, y)
     monitor.write(text)
+    monitor.setTextColor(textColor)
+    monitor.setBackgroundColor(backgroundColor)
 end
 local function drawLeftText(y, text)
     monitor.setCursorPos(1 + 1, y)
     monitor.write(text)
+    monitor.setTextColor(textColor)
+    monitor.setBackgroundColor(backgroundColor)
 end
 local function drawRightText(y, text)
     local textLength = string.len(text)
     local x = monXSize - textLength
     monitor.setCursorPos(x, y)
     monitor.write(text)
+    monitor.setTextColor(textColor)
+    monitor.setBackgroundColor(backgroundColor)
+end
+local function calcPercentage(num1, num2)
+    local percentage = num1 / num2
+    if percentage < 0.5 then
+        monitor.setTextColor(color1)
+    elseif percentage >= 0.5 and percentage < 0.9 then
+        monitor.setTextColor(color2)
+    else
+        monitor.setTextColor(color3)
+    end
 end
