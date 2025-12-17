f0940e70d9668a3f3eecfb81414e229c1572a215
Novox <113373665+DragonCraft5@users.noreply.github.com>
2025-12-17 22:09:17 +0100

    Implement button click position checking
    Added a function to check button click positions and toggle their active state.

--- controlPanel.lua
+++ controlPanel.lua
@@ -35,8 +35,22 @@ local function clear()
     term.clear()
 end
 
+local function checkClickPos(x, y)
+    for key, value in pairs(buttons) do
+        if y == value.pos and x >= 2 and x <= value.textLength + 1 then
+            value.active = not value.active
+        end
+    end
+end
 
 
+
+-- define textLength
+for key, value in pairs(buttons) do
+    value.textLength = string.len(value.text)
+end
+
+-- main
 while true do
     clear()
 
