94426be1385adbc6bc83c98895ff610413f27567
Novox <113373665+DragonCraft5@users.noreply.github.com>
2025-12-17 21:41:21 +0100

    Enhance button styles and add active state
    Added new button styles and an active button state.

--- controlPanel.lua
+++ controlPanel.lua
@@ -2,8 +2,47 @@ local sizeX, sizeY = term.getSize()
 
 local backColor = colors.black
 local textColor = colors.white
+local buttonBackColor = colors.purple
+local buttonTextColor = colors.pink
+local activeButtonBackColor = colors.pink
+local activeButtonTextColor = colors.purple
+
 
 local buttons = {
-    {pos = 2, text = "test button", reciever = 1, message = "press", toggle = false},
-    {pos = 2, text = "toggle button", reciever = 2, message = "toggle", toggle = true, active = false}
+    {pos = 2, textLength, text = "test button", reciever = 1, message = "press", toggle = false},
+    {pos = 4, textLength, text = "toggle button", reciever = 2, message = "toggle", toggle = true, active = false},
+    {pos = 6, textLength, text = "active button", reciever = 6, message = "toggle", toggle = true, active = true}
 }
+
+
+
+local function drawButton(pos, text, active)
+    if active then
+        term.setBackgroundColor(activeButtonBackColor)
+        term.setTextColor(activeButtonTextColor)
+    else
+        term.setBackgroundColor(buttonBackColor)
+        term.setTextColor(buttonTextColor)
+    end
+
+    term.setCursorPos(2, pos)
+    term.write(text)
+end
+
+local function clear()
+    term.setBackgroundColor(backColor)
+    term.setTextColor(textColor)
+    term.clear()
+end
+
+
+
+while true do
+    clear()
+
+    for key, value in pairs(buttons) do
+        drawButton(value.pos, value.text, value.active)
+    end
+
+    os.sleep(1)
+end
